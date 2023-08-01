ad7af751998b2f01bd0fda5b0bc7f0b46f1e7317
ggagne <ggagne@westminsteru.edu>
2023-08-01 13:56:27 -0600

    Added PracticeOne class

--- src/PracticeOne.java
+++ src/PracticeOne.java
@@ -1,5 +1,5 @@
 public class PracticeOne {
     public static void main(String[] args) {
-        System.out.println("Welcome to Data Struxtures!");
+        System.out.println("Welcome to Data Structures!");
     }
 }
